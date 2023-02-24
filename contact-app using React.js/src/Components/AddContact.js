30a9b1d2b3a14bcc659bf9c4d5257e9493d550c4
Amit Gurjar <122373486+banisheddisciple@users.noreply.github.com>
2023-02-24 22:43:18 +0530

    Add files via upload

---
+++ contact-app using React.js/src/Components/AddContact.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+class AddContact extends React.Component {
+    state = {
+        name: "",
+        number: "",
+    };
+    add = (e) => {
+        e.preventDefault();
+        if (this.state.name === "" || this.state.number === "") {
+            alert("this field is mandatory");
+            return;
+        }
+        this.props.addContactHandler(this.state);
+        this.setState({ name: "", number: ""});
+    }
+
+    render() {
+        return (
+            <div className="ui main">
+                <h2>Add Contact</h2>
+                <form className="ui form" onSubmit={this.add} >
+                    <div className="field">
+                        <label>Name</label>
+                        <input type="text" name="name" value={this.state.name} placeholder="Name" onChange={ (e) => this.setState({name: e.target.value })} />
+                    </div>
+                    <div className="field">
+                        <label>Number</label>
+                        <input type="text" value={this.state.number} name="number" placeholder="Number" onChange={ (e) => this.setState({number: e.target.value })} />
+                    </div>
+                    <button className="ui button blue">Add</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default AddContact;
